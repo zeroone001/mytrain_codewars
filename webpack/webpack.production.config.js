5b55f3ed5fec8c142d94c82f3a9d2c399c4e39e4
smzdm <ilylys@163.com>
2016-07-01 20:28:16 +0800

    解决提交出错的问题

--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -3,6 +3,7 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH,'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var TEM_PATH = path.resolve(ROOT_PATH, 'templates');
 module.exports = {
     entry: {
         //三个入口文件
@@ -20,9 +21,22 @@ module.exports = {
         //把入口文件里面的数组打包成verdors.js
         new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
 
+        //创建了两个HtmlWebpackPlugin的实例，生成两个页面
         new HtmlwebpackPlugin({
             title: 'Hello World app',
-
+            template: path.resolve(TEM_PATH, 'index.html'),
+            filename: 'index.html',
+            //chunks这个参数告诉插件要引用entry里面的哪几个入口
+            chunks: ['app', 'vendors'],
+            //要把script插入到标签里
+            inject: 'body'
+        }),
+        new HtmlwebpackPlugin({
+            title: 'Hello Mobile app',
+            template: path.resolve(TEM_PATH, 'mobile.html'),
+            filename: 'mobile.html',
+            chunks: ['mobile', 'vendors'],
+            inject: 'body'
         })
     ],
     //devServer:{
